5bc743e3a7c468babfa1d017700e2854f8cd2783
John Doll <dolljm@miamioh.edu>
2020-10-02 20:19:49 -0400

    Completed 4 LargestPalindrome number

--- Four_LargestPalindromeProduct/src/LargestPalindromeProduct.java
+++ Four_LargestPalindromeProduct/src/LargestPalindromeProduct.java
@@ -5,18 +5,27 @@ public class LargestPalindromeProduct {
 	}
 	
 	public static int largestPalindromeProduct() {
+		boolean value = true;
+		int largest = 0;
 		for(int i = 999; i > 0; i--) {
-			for(int j = 999; j > 0; i--) {
+			for(int j = i; j > 0; j--) {
 				String y = ((Integer)(i*j)).toString();
-				for(int k = 0; k < y.length(); k++) {
+				if(i * j < largest) 
+					break;
+				value = true;
+				for(int k = 0; k < y.length()/2; k++) {
 					if(y.charAt(k) != y.charAt(y.length() - 1 - k)) {
-						
+						value = false;
+						break;
 					}
 				}
+				if(value) {
+					largest = i*j;
+					break;
+				}
+				
 			}
 		}
-		
-		
-		return 0;
+		return largest;
 	}
 }
