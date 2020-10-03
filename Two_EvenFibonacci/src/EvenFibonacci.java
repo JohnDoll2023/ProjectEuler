515f4adade512907eb8e337e4bdeb8a5e9caf31c
John Doll <dolljm@miamioh.edu>
2020-10-03 16:50:57 -0400

    Nearly completed Problem8
    Unable to locate error. Did some more work on Problem2 but still stumped there.

--- Two_EvenFibonacci/src/EvenFibonacci.java
+++ Two_EvenFibonacci/src/EvenFibonacci.java
@@ -3,15 +3,14 @@ public class EvenFibonacci {
 	static int count = 0;
 	
 	public static void main(String[] args) {
-		fibonacci(10);
+		System.out.println(fibonacci(10));
 		System.out.println(count);
 	}
 	
 	public static int fibonacci(int n) {
 		if(n <= 1)
 			return 1;
-		if(n % 2 == 0)
-			count++;
-		return fibonacci(n - 1) + fibonacci(n - 2);
+		
+		return count += ((fibonacci(n - 1) + fibonacci(n - 2)) % 2 == 0 ? 1 : 0) ;
 	}
 }
