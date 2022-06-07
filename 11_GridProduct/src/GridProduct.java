9417a598629a9dde720c1fe25ff049a6343d253b
dolljm <dolljm@miamioh.edu>
2022-06-07 01:16:39 -0700

    attempt at question 11

---
+++ 11_GridProduct/src/GridProduct.java
@@ -0,0 +1,48 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class GridProduct {
+
+	public static void main(String[] args) {
+		try {
+			File file = new File("gridproduct.txt");
+			Scanner in = new Scanner(file);
+			System.out.println(gridProduct(in));
+		} catch (FileNotFoundException e) {
+			System.out.println("Unable to locate file.");
+		}
+	}
+	
+	public static int gridProduct(Scanner file) {
+		int [][] grid = new int[20][20];
+		int row1 = 0;
+		while(file.hasNextLine()) {
+			for (int col = 0; col < 20; col++) {
+				grid[col][row1] = file.nextInt();
+			}
+			row1++;
+		}
+		int max = 0;
+		outer: for (int row = 0; row < 20; row++) {
+			for (int col = 0; col < 20; col++) {
+				// diagonal
+				if (row <= 16 && col <= 16) {
+					max = Math.max(grid[col][row] * grid[col + 1][row + 1] * grid[col + 2][row + 2] *  grid[col + 3][row + 3], max);
+					max = Math.max(grid[col][row] * grid[col + 1][row] * grid[col + 2][row] * grid[col + 3][row], max);
+					max = Math.max(grid[col][row] * grid[col][row + 1] * grid[col][row + 2] * grid[col][row + 3], max);	
+				} else if (col <= 16) {
+					// right
+					max = Math.max(grid[col][row] * grid[col + 1][row] * grid[col + 2][row] * grid[col + 3][row], max);
+				} else if (row <= 16) {
+					// down
+					max = Math.max(grid[col][row] * grid[col][row + 1] * grid[col][row + 2] * grid[col][row + 3], max);
+				} else {
+					break outer;
+				}
+			}
+		}
+		return max;
+	}
+
+}
