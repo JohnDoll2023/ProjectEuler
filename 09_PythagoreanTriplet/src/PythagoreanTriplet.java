bf2a2c6e37722573eb69227719185b93fde3bddf
dolljm <dolljm@miamioh.edu>
2022-06-09 00:01:31 -0700

    Update PythagoreanTriplet.java

--- 09_PythagoreanTriplet/src/PythagoreanTriplet.java
+++ 09_PythagoreanTriplet/src/PythagoreanTriplet.java
@@ -1,9 +1,18 @@
-
 public class PythagoreanTriplet {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println(pythagoreanTriplet());
+	}
+	
+	public static int pythagoreanTriplet() {
+		int product = 0;
+		int a = 0, b = 0, c = 0;
+		// a + b > c
+		// a^2 + b^2 = c^2
+		// a + b + c = 1000
+		
+		
+		return product;
 	}
 
 }
