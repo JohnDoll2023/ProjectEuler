c349da74f54ad73d7f388f708a89a04b218401d8
John Doll <jdollmu@gmail.com>
2025-01-14 23:35:51 -0500

    cleanup, i know it could be better but im too busy watching the aussie open

--- 43_SubstringDivisibility/src/SubstringDivisibility.java
+++ 43_SubstringDivisibility/src/SubstringDivisibility.java
@@ -1,83 +1,73 @@
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.PrintStream;
-
 public class SubstringDivisibility {
-    public static void main(String [] args) throws FileNotFoundException {
-        // make file for printing debug
-        File file = new File("debug1.txt");
-        // print to file
-        System.setOut(new PrintStream(file));
-
+    public static void main(String [] args) {
         // set numbers
-        long i = 1406357289L; // 1,000,000,000
+        long number = 1023456789L; // 1,023,456,789
         long sum = 0;
-        long number;
+        long check;
+        long num;
 
         // while loop until we hit 10,000,000,000
-        while (i < 10000000000L) {
-            number = i;
-
-            long check = number / 1000000;
-            long two = check % 1000; //1  998  876543
+        while (number < 9876543210L) {
+            check = number / 1000000;
+            num = check % 1000; //1  998  876543
             // 2 check
-            if (two % 2 == 0) {
+            if (num % 2 == 0) {
                 check = number / 100000;
-                long three = check % 1000; //19  988  76543
+                num = check % 1000; //19  988  76543
                 // 3 check
-                if (three % 3 == 0) {
+                if (num % 3 == 0) {
                     check = number / 10000;
-                    long five = check % 1000; //199  887  6543
+                    num = check % 1000; //199  887  6543
                     // 5 check
-                    if (five % 5 == 0) {
+                    if (num % 5 == 0) {
                         check = number / 1000;
-                        long seven = check % 1000; //1998  876  543
+                        num = check % 1000; //1998  876  543
                         // 7 check
-                        if (seven % 7 == 0) {
+                        if (num % 7 == 0) {
                             check = number / 100;
-                            long eleven = check % 1000; //19988  765  43
+                            num = check % 1000; //19988  765  43
                             // 11 check
-                            if (eleven % 11 == 0) {
-                                check = number / 10; // 199887  654  3
-                                long thirteen = check % 1000;
+                            if (num % 11 == 0) {
+                                check = number / 10; //199887  654  3
+                                num = check % 1000;
                                 // 13 check
-                                if (thirteen % 13 == 0) {
-                                    check = number; // 1998876  543
-                                    long seventeen = check % 1000;
+                                if (num % 13 == 0) {
+                                    check = number; //1998876  543
+                                    num = check % 1000;
                                     // 17 check
-                                    if (seventeen % 17 == 0) {
-                                        sum += checkNumberForAdd(i);
+                                    if (num % 17 == 0) {
+                                        sum += checkNumberForAdd(number);
                                     }
-                                    i++;
+                                    number++;
                                 } else {
                                     // 13 remediation
-                                    i = (check + (13 - (thirteen % 13))) * 10;
-                                    i = checkIfTenDigits(i);
+                                    number = (check + (13 - (num % 13))) * 10;
+                                    number = checkIfTenDigits(number);
                                 }//end 13 check
                             } else {
                                 // 11 remediation
-                                i = (check + (11 - (eleven % 11))) * 100;
-                                i = checkIfTenDigits(i);
+                                number = (check + (11 - (num % 11))) * 100;
+                                number = checkIfTenDigits(number);
                             }//end 11 check
                         } else {
                             // 7 remediation
-                            i = (check + (7 - (seven % 7))) * 1000;
-                            i = checkIfTenDigits(i);
+                            number = (check + (7 - (num % 7))) * 1000;
+                            number = checkIfTenDigits(number);
                         }//end 7 check
                     } else {
                         // 5 remediation
-                        i = (check + (5 - (five % 5))) * 10000;
-                        i = checkIfTenDigits(i);
+                        number = (check + (5 - (num % 5))) * 10000;
+                        number = checkIfTenDigits(number);
                     }//end 5 check
                 } else {
                     // 3 remediation
-                    i = (check + (3 - (three % 3))) * 100000;
-                    i = checkIfTenDigits(i);
+                    number = (check + (3 - (num % 3))) * 100000;
+                    number = checkIfTenDigits(number);
                 }//end 3 check
             } else {
                 // 2 remediation
-                i = (check + 1) * 1000000;
-                i = checkIfTenDigits(i);
+                number = (check + 1) * 1000000;
+                number = checkIfTenDigits(number);
             }//end 2 check
         }// end while loop
         System.out.println("Sum: " + sum);
@@ -93,10 +83,8 @@ public class SubstringDivisibility {
     public static long checkNumberForAdd(long i) {
         // check that each digit 0-9 is in the number
         String str = Long.toString(i);
-        System.out.println("Checking number: " + i);
         for (int j = 0; j < 10; j++) {
             if (!str.contains(Integer.toString(j))) {
-                System.out.println("Failed on: " + j);
                 return 0;
             }
         }
